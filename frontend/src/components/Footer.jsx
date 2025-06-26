7cb2757c80d911958705b57844e14d13975b14aa
unknown <saqibmir580@gmail.com>
2025-06-25 22:29:56 -0700

    final

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+const Footer = () => {
+  return (
+    <>
+      <div>
+        <h1>hello world</h1>
+      <p>bye</p>
+      </div>
+    </>
+  );
+};
+export default Footer;
